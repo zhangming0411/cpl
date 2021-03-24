affd1fdbffb1e788b59672bd09f2f27d620d32a7
Ming.zhang <zhangming0411@foxmail.com>
2021-03-24 22:40:04 +0800

    ex1-19

---
+++ chapter1/ex1-19.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+char *reverse(char s[]);
+
+int main(int argc, char const *argv[])
+{
+    char c, *dst;
+    char s[MAXLINE];
+    int i = 0;
+    while ((c = getchar()) != EOF)
+    {
+        s[i] = c;
+        if ((c == '\n') || (i >= MAXLINE)) {
+            s[i] = '\0';
+            // printf("%s", s);
+            reverse(s);
+            printf("%s\n", s);
+            i = 0;
+            continue;
+        }
+        i++;
+    }
+    return 0;
+}
+
+char *reverse(char s[])
+{
+    int i=0, j=0;
+    char c;
+    char dst[MAXLINE];
+    while ((dst[i] = s[i]) != '\0')
+        i++;
+    // printf("i=%d\n", i);
+    if (i > 0) {
+        for (j = 0; j < i; j++) {
+            s[j] = dst[i-1-j];
+        }
+        s[j] = '\0';
+    }
+    return s;
+}
